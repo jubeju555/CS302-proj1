93f737c4cb6e80d9f482d2efd24d46d43eabeba2
judah <judahjosh@gmail.com>
2025-01-23 08:31:14 -0500

    new file:   proj1/.vscode/settings.json 	modified:   proj1/lib_info.cpp

--- proj1/lib_info.cpp
+++ proj1/lib_info.cpp
@@ -1,25 +1,91 @@
 // hello judah
+// checking if pushed
 
 #include <iostream>
 #include <vector>
 #include <string>
 #include <sstream>
+#include <fstream>
 
 using namespace std;
+struct song
+{
+    int tracknum;
+    string title;
+    string length;
+};
+struct album
+{
+    string title;
+    double length;
+    vector<song> tracks;
 
-string removeSpaces(string s){
-    for(int i=0; i< string.length(); i++){
-        if(string[i]=='_'){
-            string[i] = ' ';
-        }
+    int getTotalSeconds();
+};
+// Calculate and return the total number of 
+// seconds of all of the tracks in an album
+int album::getTotalSeconds(){
+    int total_seconds = 0;
+    int minutes = 0;
+    int seconds = 0;
+    for(int i=0; i<tracks.length(); i++)
+    {
+        stringstream ss(tracks[i]);
+        getline(ss, minutes, ':');
+        getline(ss, seconds);
+        total_seconds += (minutes*60)+seconds;
+    }
+    return total_seconds;
+}
+
+struct artist
+{
+    string name;
+    vector<album> albums;
+};
+
+string removeSpaces(string s)
+{
+    // for(int i=0; i< s.length(); i++){
+    //     if(s[i] == '_'){
+    //         s[i] = ' ';
+    //     }
+    // }
+
+    string new_string = "";
+    stringstream ss(s);
+    while (ss.good()){ //used Geeks4Geeks to referesh on stringstreams
+        string word = "";
+        getline(ss, word, '_');
+        //if formatting gets weird, add to a vector and parse that way
+        new_string = new_string + word + " ";
     }
 }
-void storefile(string filename){
-    
-    return lines;
+
+void storefile(string filename)
+{
+    // read the file in and store it, (posibly in vector)
+    ifstream fileIn(filename);
+    string line = "";
+    if(fileIn.is_open()){
+        while(getline(fileIn, line)){
+            
+        }
+    }
+    return;
 }
 
-int main(){
+void organizefile(string filename)
+{
+    return;
+}
+// print file accordngi to the format
+void printfile(string filename)
+{
+    return;
+}
+int main()
+{
 
     return 0;
 }
