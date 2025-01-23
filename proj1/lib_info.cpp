5f1e1f7001e85fa16cc3f7fbf0f004a8be163c1f
judah <judahjosh@gmail.com>
2025-01-22 20:49:21 -0500

    modified:   proj1/lib_info.cpp

--- proj1/lib_info.cpp
+++ proj1/lib_info.cpp
@@ -1,4 +1,25 @@
-// hello keaton
 // hello judah
 
 #include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+string removeSpaces(string s){
+    for(int i=0; i< string.length(); i++){
+        if(string[i]=='_'){
+            string[i] = ' ';
+        }
+    }
+}
+void storefile(string filename){
+    
+    return lines;
+}
+
+int main(){
+
+    return 0;
+}
